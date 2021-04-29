5dce1481dc6519d844cd20df7fbafbd525d6f1cc
Julian Infante <julianinfante50@gmail.com>
2021-04-29 10:53:50 -0400

    RetServer - Usuario

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');    
 
+const mongoose = require('mongoose');
+
 const express = require('express');
 const app = express();
 
@@ -11,32 +13,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./rutas/usuarios')  );
 
-app.get('/usuario', function (req, res) {
-    res.json('get Usuario')
-})
 
-app.post('/usuario', function (req, res) {
 
-    let body = req.body
 
-    res.json({
-        body
-    });
-});
+ mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
 
-app.put('/usuario/:id', function (req, res) {
+          if ( err ) throw err;
+          console.log('Base de datos ONLINE');
 
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario')
-})
+ });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto: ', process.env.PORT);
